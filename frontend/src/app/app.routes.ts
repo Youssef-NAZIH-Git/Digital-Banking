e624463f3f4da81a0f87e72ef8b0c5a35fe94a3d
Youssef-YN <nazihyoussef05@gmail.com>
2026-05-10 21:43:34 +0100

    added login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { CustomersComponent } from './components/customers/customers.component';
 import { AccountsComponent } from './components/accounts/accounts.component';
 import { NewCustomerComponent } from './components/customers/new-customer/new-customer.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: "customers", component: CustomersComponent},
     {path: "accounts", component: AccountsComponent},
-    {path: "new-customer", component: NewCustomerComponent}
+    {path: "new-customer", component: NewCustomerComponent},
+    {path: 'login', component: LoginComponent},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'admin', redirectTo: 'customers', pathMatch: 'full'}
 ];
---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  handleLogin() {
+    if (this.loginForm.invalid) return;
+
+    const { username, password } = this.loginForm.value;
+
+    console.log('auth', { username, password });
+    this.router.navigateByUrl('/admin');
+  }
+}
